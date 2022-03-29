6d86c56c7cad30c3fd3bcb4d5b0c84bcbba1c4ae
Apisit Sangkrajang <apisitsangkrajang@gmail.com>
2022-03-29 12:01:49 +0700

    Login Css

--- src/Login.svelte
+++ src/Login.svelte
@@ -21,14 +21,36 @@
   }
 </script>
 
-<h1>Login</h1>
+<div id='login-container'>
+  <h1>Login</h1>
 
-<input bind:value={username} placeholder="Username" />
-<input type="password" bind:value={pin} placeholder="Password" />
-<button on:click={checkLogin}>Log In</button>
-<button on:click={forgot}>forgot password</button>
+  <input bind:value={username} placeholder="Username" />
+  <input type="password" bind:value={pin} placeholder="Password" />
+  <div id='login-btn-container'>
+    <button  on:click={checkLogin}>Log In</button>
+    <button  on:click={forgot}>forgot password</button>
+  </div>
+</div>
 
 <style>
+  :global(body){
+    background-color: antiquewhite;
+  }
+  #login-container {
+    height:100%;
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #login-btn-container {
+    display: inline;
+  }
+
   :global(input) {
     display: flex;
     margin: 6px;
