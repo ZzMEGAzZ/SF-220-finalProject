514ddc3dc3a0ad0804b81e6066adc34f61c28cbb
Apisit Sangkrajang <apisitsangkrajang@gmail.com>
2022-04-05 12:28:34 +0700

    web google firebase

--- src/App.svelte
+++ src/App.svelte
@@ -13,6 +13,28 @@
   import Addadmin from "./Addadmin.svelte";
   import Removeadmin from "./Removeadmin.svelte";
   import Detail from "./Detail.svelte";
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "firebase/app";
+  import { getAnalytics } from "firebase/analytics";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCb0yKSZNCYMUmtONpHi_8f5KQTUVv8Nhk",
+    authDomain: "sf220-last-project.firebaseapp.com",
+    projectId: "sf220-last-project",
+    storageBucket: "sf220-last-project.appspot.com",
+    messagingSenderId: "519567761335",
+    appId: "1:519567761335:web:21996f7a3300e84f715232",
+    measurementId: "G-R68XQY6Z69"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 </script>
 
 {#if !$islogin && !($mode == "forgot")}
