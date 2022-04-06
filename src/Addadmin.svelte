0369ba496a668af2e715657a73f36368c89a952e
NPSN PUANGBUBPA <99357219+earnearn2354@users.noreply.github.com>
2022-04-06 13:29:15 +0000

    เพิ่ม css หน้า addadmin

--- src/Addadmin.svelte
+++ src/Addadmin.svelte
@@ -52,54 +52,60 @@
   }
 </script>
 
-<h1>รายละเอียดวิชาที่ต้องการเพิ่ม</h1>
-{#if showmode == true}
-  <form>
-    รหัสวิชา:<input
-      type="text"
-      minlength="5"
-      maxlength="5"
-      on:input={(e) => (newname = e.target.value)}
-    />
-    ชื่อวิชา:<input
-      type="text"
-      maxlength="30"
-      on:input={(e) => (newsubject = e.target.value)}
-    />
-    จำนวนโควต้า:<input
-      type="number"
-      min="0"
-      max="100"
-      on:input={(e) => (newtotal = e.target.value)}
-    />
-    หน่วยกิต:
-    <input
-      type="number"
-      min="0"
-      max="3"
-      on:input={(e) => (newcredit = e.target.value)}
-    />
-    ชื่ออาจารย์ผู้สอน:<input
-      type="text"
-      maxlength="30"
-      on:input={(e) => (newTeacher = e.target.value)}
-    />
-    เพิ่มเติม:
-    <input
-      type="text"
-      maxlength="5"
-      on:input={(e) => (newshort_description = e.target.value)}
-    />
-    รายละเอียดวิชา:<input
-      class="resize-box"
-      type="text"
-      maxlenght="100"
-      on:input={(e) => (newdescription = e.target.value)}
-    />
+<div class="container">
+  <div class="model">
+    <h1>รายละเอียดวิชาที่ต้องการเพิ่ม</h1>
+    <div class="terms">
+    {#if showmode == true}
+      <form>
+        รหัสวิชา:<input
+          type="text"
+          minlength="5"
+          maxlength="5"
+          on:input={(e) => (newname = e.target.value)}
+        />
+        ชื่อวิชา:<input
+          type="text"
+          maxlength="30"
+          on:input={(e) => (newsubject = e.target.value)}
+        />
+        จำนวนโควต้า:<input
+          type="number"
+          min="0"
+          max="100"
+          on:input={(e) => (newtotal = e.target.value)}
+        />
+        หน่วยกิต:
+        <input
+          type="number"
+          min="0"
+          max="3"
+          on:input={(e) => (newcredit = e.target.value)}
+        />
+        ชื่ออาจารย์ผู้สอน:<input
+          type="text"
+          maxlength="30"
+          on:input={(e) => (newTeacher = e.target.value)}
+        />
+        เพิ่มเติม:
+        <input
+          type="text"
+          maxlength="5"
+          on:input={(e) => (newshort_description = e.target.value)}
+        />
+        รายละเอียดวิชา:<input
+          class="resize-box"
+          type="text"
+          maxlenght="100"
+          on:input={(e) => (newdescription = e.target.value)}
+        />
 
-    <button on:click={() => show_results()}>ยืนยันวิชาที่ต้องการเพิ่ม</button>
-  </form>
-{/if}
+          <button class="button" on:click={() => show_results()}>ยืนยันวิชาที่ต้องการเพิ่ม</button>
+        </form>
+    {/if}
+    </div>
+  </div>
+</div>
 
 {#if showmode == false}
   <table>
@@ -130,4 +136,48 @@
 {/if}
 
 <style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .model {
+    max-width: 50%;
+    padding: 1rem 2rem;
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+    background-color: rgba(255, 255, 255, 0.4);
+    border-radius: 0.25rem;
+    box-shadow: 0px 10px 30px 5px rgba(87,85,101,.2);
+    
+  }
+
+  .button {
+    margin-top: auto;
+    padding: 10px 20px;
+    font-size: 15px;
+    text-align: center;
+    cursor: pointer;
+    outline: none;
+    background-color: #d3d3d3;
+    border: none;
+    border-radius: 15px;
+    box-shadow: 0 9px rgb(228, 228, 228);
+  }
+
+  .button:hover {
+    background-color: #b5b5b5;
+  }
+
+  .button:active {
+    background-color: #b5b5b5;
+    box-shadow: 0 5px rgb(228, 228, 228);
+    transform: translateY(4px);
+  }
 </style>
