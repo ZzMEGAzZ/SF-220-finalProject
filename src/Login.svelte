85944b5a09834f81b18a07baedffa720b7ff4522
PANUWAT MANGKANG <87230490+aodaod0987@users.noreply.github.com>
2022-04-08 20:14:57 +0000

    ย้อนversion 2

--- src/Login.svelte
+++ src/Login.svelte
@@ -19,22 +19,13 @@
     $islogin = false;
     $mode = "forgot";
   }
+  
 </script>
-
 <div id="login-container">
   <h1>Soft-fell</h1>
 
   <input bind:value={username} placeholder="ชื่อบัญชี" />
-  <input
-    type="password"
-    bind:value={pin}
-    placeholder="รหัสผ่าน"
-    on:keypress={(e) => {
-      if (e.key == "Enter") {
-        checkLogin();
-      }
-    }}
-  />
+  <input type="password" bind:value={pin} placeholder="รหัสผ่าน" />
   <div id="login-btn-container">
     <button on:click={() => checkLogin()}>เข้าสู่ระบบ</button>
     <p>|</p>
@@ -49,7 +40,7 @@
     margin: 0;
     padding: 0;
     font-family: "prompt", sans-serif;
-    background-image: url("https://cdn.discordapp.com/attachments/959342189224271872/960768895021637712/BG_center_wave5.png");
+		background-image: url("https://cdn.discordapp.com/attachments/959342189224271872/960768895021637712/BG_center_wave5.png");
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-position: center;
@@ -88,16 +79,6 @@
   }
 
   #login-btn-container button:hover {
-    color: #ff2361;
-    transition: 0.1s;
-  }
-
-  :global(input) {
-    display: flex;
-    margin: 6px;
-  }
-</style>
- #login-btn-container button:hover {
     color: #ff2361;
     transition: 0.1s;
   }
