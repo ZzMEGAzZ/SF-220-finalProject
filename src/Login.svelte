f05b1bec53e6534d1d456a41bc872b2af55da183
Apisit Sangkrajang <85662757+ZzMEGAzZ@users.noreply.github.com>
2022-04-16 14:33:41 +0000

    time

--- src/Login.svelte
+++ src/Login.svelte
@@ -19,6 +19,22 @@
     $islogin = false;
     $mode = "forgot";
   }
+
+  function startTime() {
+        const today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
+        setTimeout(startTime, 1000);
+      }
+      
+      function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+      }
 </script>
 
 <main on:load={startTime()} />
