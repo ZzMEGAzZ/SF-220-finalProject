fcb0164768779feb9dcf817bf9bbd0dd28406a80
Apisit Sangkrajang <85662757+ZzMEGAzZ@users.noreply.github.com>
2022-04-16 16:35:20 +0000

    cancel back

--- src/Addadmin.svelte
+++ src/Addadmin.svelte
@@ -16,7 +16,7 @@
       alert("กรุณากรอกเป็นจำนวนเต็ม");
     } else if (
       newcredit >= 0 &&
-      newtotal >= 1 &&
+      newtotal >= 0 &&
       newcredit <= 3 &&
       newtotal <= 100
     ) {
@@ -30,7 +30,7 @@
   }
 
   function confirm() {
-    if (newcredit >= 0 && newtotal >= 1 && newcredit <= 3 && newtotal <= 100) {
+    if (newcredit >= 0 && newtotal >= 0 && newcredit <= 3 && newtotal <= 100) {
       $subjects.push({
         name: newname,
         subject: newsubject,
@@ -54,17 +54,6 @@
       newdescription = "";
       mode.set("allsubjects");
     }
-  }
-
-   function back() {
-    showmode = true;
-    newname = "";
-    newsubject = "";
-    newtotal = "";
-    newcredit = "";
-    newTeacher = "";
-    newshort_description = "";
-    newdescription = "";
   }
 </script>
 
@@ -93,7 +82,7 @@
           <div class="sub-3">
           จำนวนโควต้า:<input
             type="number"
-            min="1"
+            min="0"
             max="100"
             step="1"
             on:input={(e) => (newtotal = e.target.value)}
@@ -163,7 +152,6 @@
         </tr>
       </tbody>
     </table>
-    <button on:click={() => back()}>ย้อนกลับ</button>
     <button class="button1" style="vertical-align: middle;" on:click={() => confirm()}
       ><span>ยืนยันวิชาที่ต้องการเพิ่ม</span></button
     >
