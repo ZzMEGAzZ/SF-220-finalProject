cc1df6c350d35a63e4848ec84f24c353e4360a2f
Apisit Sangkrajang <85662757+ZzMEGAzZ@users.noreply.github.com>
2022-04-17 06:45:36 +0000

    new addadmin

--- src/Addadmin.svelte
+++ src/Addadmin.svelte
@@ -10,28 +10,61 @@
   import { subjects, accounts, account, mode } from "./stores.js";
 
   function show_results() {
-    if (newname == "" || newtotal == "" || newcredit == "" || newname.length < 5){
-      alert("กรุณากรอกรายละเอียดให้ครบถ้วน");
+    if ((newcredit < 0 || newcredit > 3 || newcredit == "") && (newtotal < 1 || newtotal > 100 || newtotal == "") && (newname == "" || newname.length < 5)) {
+      alert("กรุณากรอกรายละเอียดให้ถูกต้อง");
+      document.getElementById("total-sub").style.display = "block";
+      document.getElementById("credit-sub").style.display = "block";
+      document.getElementById("name-sub").style.display = "block";
+    } else if ((newcredit < 0 || newcredit > 3 || newcredit == "") && (newtotal < 1 || newtotal > 100 || newtotal == "")) {
+      alert("กรุณากรอกรายละเอียดให้ถูกต้อง");
+      document.getElementById("total-sub").style.display = "block";
+      document.getElementById("credit-sub").style.display = "block";
+      document.getElementById("name-sub").style.display = "none";
+    } else if ((newcredit < 0 || newcredit > 3 || newcredit == "") && (newname == "" || newname.length < 5)) {
+      alert("กรุณากรอกรายละเอียดให้ถูกต้อง");
+      document.getElementById("total-sub").style.display = "none";
+      document.getElementById("credit-sub").style.display = "block";
+      document.getElementById("name-sub").style.display = "block";
+    } else if ((newtotal < 1 || newtotal > 100 || newtotal == "") && (newname == "" || newname.length < 5)) {
+      alert("กรุณากรอกรายละเอียดให้ถูกต้อง");
+      document.getElementById("total-sub").style.display = "block";
+      document.getElementById("credit-sub").style.display = "none";
+      document.getElementById("name-sub").style.display = "block";
     } else if (!Number.isInteger(+newtotal) || !Number.isInteger(+newcredit)) {
-      alert("กรุณากรอกเป็นจำนวนเต็ม");
-    } else if (
-      newcredit >= 0 &&
-      newtotal >= 1 &&
-      newcredit <= 3 &&
-      newtotal <= 100
-    ) {
-      document.getElementById("container").style.display = "none";
-      document.getElementById("container").style.height = "0";
+      alert("กรุณากรอกเป็นจำนวนเต็ม");    
+    } else if (newname == "" || newname.length < 5) {
+      alert("กรุณากรอกรายละเอียดให้ถูกต้อง");
+      document.getElementById("name-sub").style.display = "block";
+      document.getElementById("total-sub").style.display = "none";
+      document.getElementById("credit-sub").style.display = "none";
+    } else if (newcredit < 1 || newcredit > 4 || newcredit == "")  {
+      alert("กรุณากรอกรายละเอียดให้ถูกต้อง");
+      document.getElementById("credit-sub").style.display = "block";
+      document.getElementById("total-sub").style.display = "none";
+      document.getElementById("name-sub").style.display = "none";
+    } else if (newtotal < 1 || newtotal > 100 || newtotal == "")  {
+      alert("กรุณากรอกรายละเอียดให้ถูกต้อง");
+      document.getElementById("total-sub").style.display = "block";
+      document.getElementById("credit-sub").style.display = "none";
+      document.getElementById("name-sub").style.display = "none";
+
+    } else if (newcredit >= 0 && newtotal >= 1 && newcredit <= 3 && newtotal <= 100) {
+      document.getElementById("total-sub").style.display = "none";
+      document.getElementById("credit-sub").style.display = "none";
+      document.getElementById("name-sub").style.display = "none";
       document.getElementById("model").style.display = "none";
       showmode = false;
     } else {
       alert("กรุณากรอกรายละเอียดให้ถูกต้อง");
+      document.getElementById("total-sub").style.display = "none";
+      document.getElementById("credit-sub").style.display = "none";
+      document.getElementById("name-sub").style.display = "none";
     }
     for (let i = 0; i < $subjects.length; i++) {
       if ($subjects[i].name == newname) {
         alert("มีรายวิชาดังกล่าวแล้ว");
         duplicate();
-    }
+      }
     }
   }
 
@@ -62,20 +95,17 @@
     }
   }
 
-   function back() {
+  function back() {
     showmode = true;
-    document.getElementById("container").style.display = "flex";
     document.getElementById("model").style.display = "flex";
   }
 
   function duplicate() {
     newname = "";
     showmode = true;
-    document.getElementById("container").style.display = "flex";
     document.getElementById("model").style.display = "flex";
     document.getElementById("name").value = newname;
   }
-
 </script>
 
 <h1>รายละเอียดวิชาที่ต้องการเพิ่ม</h1>
@@ -84,69 +114,89 @@
   <div id="model">
     {#if showmode == true}
       <div class="terms">
-          <div class="highsub-1">
+        <div class="highsub-1">
           <div class="sub-1">
-          รหัสวิชา:<input id="name"
-            type="text"
-            minlength="5"
-            maxlength="5"
-            value="{newname}"
-            on:input={(e) => (newname = e.target.value)}
-          /></div>
+            <div class="sub-name">
+            รหัสวิชา: <p id="name-sub">*กรุณาใส่ให้ครบ <br> 5 ตำแหน่ง </p></div><input
+              id="name"
+              type="text"
+              minlength="5"
+              maxlength="5"
+              value={newname}
+              on:input={(e) => (newname = e.target.value)}
+            />
+          </div>
           <div class="sub-2">
-          ชื่อวิชา:<input id="subject"
-            type="text"
-            maxlength="30"
-            value="{newsubject}"
-            on:input={(e) => (newsubject = e.target.value)}
-          /> </div> </div>
-          <div class="highsub-2">
+            ชื่อวิชา:<input
+              id="subject"
+              type="text"
+              maxlength="30"
+              value={newsubject}
+              on:input={(e) => (newsubject = e.target.value)}
+            />
+          </div>
+        </div>
+        <div class="highsub-2">
           <div class="sub-3">
-          จำนวนโควต้า:<input id="total"
-            type="number"
-            min="1"
-            max="100"
-            step="1"
-            value="{newtotal}"
-            on:input={(e) => (newtotal = e.target.value)}
-          /> </div>
+            <div class="sub-total">
+            จำนวนโควต้า:<p id="total-sub">*ขั้นต่ำ 1 คน <br> ไม่เกิน 100 คน</p></div><input
+              id="total"
+              type="number"
+              min="1"
+              max="100"
+              step="1"
+              value={newtotal}
+              on:input={(e) => (newtotal = e.target.value)}
+            />
+          </div>
           <div class="sub-4">
-          หน่วยกิต:
-          <input id="credit"
-            type="number"
-            min="0"
-            max="3"
-            step="1"
-            value="{newcredit}"
-            on:input={(e) => (newcredit = e.target.value)}
-          /> </div> </div>
-          <div class="highsub-3">
+            <div class="sub-credit">
+            หน่วยกิต:<p id="credit-sub">*ขั้นต่ำ 1 หน่วย <br> ไม่เกิน 3 หน่วย</p></div><input
+              id="credit"
+              type="number"
+              min="0"
+              max="3"
+              step="1"
+              value={newcredit}
+              on:input={(e) => (newcredit = e.target.value)}
+            />
+          </div>
+        </div>
+        <div class="highsub-3">
           <div class="sub-5">
-          ชื่ออาจารย์ผู้สอน:<input id="teacher"
-            type="text"
-            maxlength="30"
-            value="{newTeacher}"
-            on:input={(e) => (newTeacher = e.target.value)}
-          /> </div>
+            ชื่ออาจารย์ผู้สอน:<input
+              id="teacher"
+              type="text"
+              maxlength="30"
+              value={newTeacher}
+              on:input={(e) => (newTeacher = e.target.value)}
+            />
+          </div>
           <div class="sub-6">
-          เพิ่มเติม:
-          <input id="short_description"
-            type="text"
-            maxlength="5"
-            value="{newshort_description}"
-            on:input={(e) => (newshort_description = e.target.value)}
-          /> </div> </div>
-          <div class="sub-7">
-          รายละเอียดวิชา:<textarea id="description"
+            เพิ่มเติม:
+            <input
+              id="short_description"
+              type="text"
+              maxlength="5"
+              value={newshort_description}
+              on:input={(e) => (newshort_description = e.target.value)}
+            />
+          </div>
+        </div>
+        <div class="sub-7">
+          รายละเอียดวิชา:<textarea
+            id="description"
             type="text"
             maxlength="300"
-            value="{newdescription}"
+            value={newdescription}
             on:input={(e) => (newdescription = e.target.value)}
-          /> </div>
-          <div class="sub-8">
+          />
+        </div>
+        <div class="sub-8">
           <button class="button" on:click={() => show_results()}
             >ยืนยันวิชาที่ต้องการเพิ่ม</button
-          > </div>
+          >
+        </div>
       </div>
     {/if}
   </div>
@@ -179,13 +229,32 @@
       </tbody>
     </table>
     <button on:click={() => back()}>ย้อนกลับ</button>
-    <button class="button1" style="vertical-align: middle;" on:click={() => confirm()}
-      ><span>ยืนยันวิชาที่ต้องการเพิ่ม</span></button
+    <button
+      class="button1"
+      style="vertical-align: middle;"
+      on:click={() => confirm()}><span>ยืนยันวิชาที่ต้องการเพิ่ม</span></button
     >
   {/if}
 </div>
 
 <style>
+  p {
+    font-size: 10px;
+    color: tomato;
+  }
+
+  #name-sub {
+    display: none;
+  }
+
+  #total-sub {
+    display: none;
+  }
+
+  #credit-sub {
+    display: none;
+  }
+
   textarea {
     resize: both;
     min-width: 200px;
@@ -201,11 +270,17 @@
   }
 
   div#container {
+    height: auto;
+    width: 100%;
     display: flex;
     justify-content: center;
   }
 
-  form {
+  #model {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
     background-color: white;
     padding: 20px;
     border-radius: 5px;
@@ -281,7 +356,7 @@
     transition: all 0.5s;
     cursor: pointer;
     margin: 5px;
-    box-shadow: 0 9px rgb(228, 228, 228)
+    box-shadow: 0 9px rgb(228, 228, 228);
   }
   .button1 span {
     cursor: pointer;
@@ -290,7 +365,7 @@
     transition: 0.5s;
   }
   .button1 span:after {
-    content: '\00bb';
+    content: "\00bb";
     position: absolute;
     opacity: 0;
     top: 0;
@@ -304,7 +379,7 @@
     opacity: 1;
     right: 0;
   }
-  
+
   .highsub-1,
   .highsub-2,
   .highsub-3 {
@@ -324,7 +399,7 @@
     margin-top: 10px;
   }
 
-  .sub-3, 
+  .sub-3,
   .sub-4 {
     display: flex;
     justify-content: left;
@@ -340,5 +415,4 @@
     justify-content: center;
     margin-top: 10px;
   }
-  
 </style>
