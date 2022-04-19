eaedb0dcb5d1f278c8d9635a14874a751d0492af
NPSN PUANGBUBPA <99357219+earnearn2354@users.noreply.github.com>
2022-04-19 10:06:02 +0000

    css ปุ่มดูรายวิชา

--- src/All.svelte
+++ src/All.svelte
@@ -58,7 +58,7 @@
           <p id="index">{index + 1}</p>
           {#if !isadmin()}
             <div id="description">
-              <button on:click={() => description(name)}>{name}</button>
+              <button class="glow-on-hover" on:click={() => description(name)}>{name}</button>
             </div>
           {:else}
             <p id="name">{name}</p>
@@ -231,5 +231,77 @@
     margin: 10px;
     padding: 40px;
     text-align: center;
+  }
+  .glow-on-hover {
+    width: auto;
+    height: 42px;
+    padding: 10px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: rgba(190, 220, 239, 1) 42%;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+  }
+
+  .glow-on-hover:before {
+    content: "";
+    background: linear-gradient(
+      180deg,
+      rgba(17, 34, 49, 1) 10%,
+      rgba(30, 71, 112, 1) 22%,
+      rgba(69, 117, 165, 1) 32%,
+      rgba(190, 220, 239, 1) 42%
+    );
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+    border-radius: 10px;
+  }
+
+  .glow-on-hover:active {
+    color: #000;
+  }
+
+  .glow-on-hover:active:after {
+    background: transparent;
+  }
+
+  .glow-on-hover:hover:before {
+    opacity: 1;
+  }
+
+  .glow-on-hover:after {
+    z-index: -1;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+  }
+
+  @keyframes glowing {
+    0% {
+      background-position: 0 0;
+    }
+    50% {
+      background-position: 400% 0;
+    }
+    100% {
+      background-position: 0 0;
+    }
   }
 </style>
